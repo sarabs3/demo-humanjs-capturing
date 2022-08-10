b6af3f2bbc7f3fec73607ac7fb099486f65f12fe
sarabs3js <46516570+sarabs3js@users.noreply.github.com>
2022-08-10 20:09:39 +0530

    updated next config

--- next.config.js
+++ next.config.js
@@ -1,26 +1,7 @@
 /** @type {import('next').NextConfig} */
-
-module.exports = {
-  reactStrictMode: false,
-  compress: false,
-  poweredByHeader: false,
-  generateEtags: true,
-  distDir: '.next',
-  httpAgentOptions: {
-    keepAlive: true,
-  },
-  devIndicators: {
-    buildActivity: true,
-    buildActivityPosition: 'bottom-right',
-  },
-  images: {
-    loader: 'custom',
-  },
-  basePath: process.env.NODE_ENV === 'production' ? '/human-next/out' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/human-next/out/' : '',
-  exportPathMap: async function (defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
-    return {
-      '/': { page: '/' },
-    }
-  },
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
 }
+
+module.exports = nextConfig
